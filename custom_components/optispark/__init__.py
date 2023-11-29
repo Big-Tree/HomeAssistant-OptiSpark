c39e996c9595216698fb0d133f8191f3533c3b1f
William Murphy <william.murphy333@gmail.com>
2023-11-29 18:19:02 +0000

    You can now select the climate entity in the config flow
    Updated lambda call for house_config argument

--- custom_components/optispark/__init__.py
+++ custom_components/optispark/__init__.py
@@ -9,6 +9,7 @@ from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import Platform
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
+from homeassistant.helpers import entity_registry
 
 from .api import OptisparkApiClient
 from .const import DOMAIN
@@ -25,13 +26,15 @@ PLATFORMS: list[Platform] = [
 # https://developers.home-assistant.io/docs/config_entries_index/#setting-up-an-entry
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up this integration using UI."""
+    climate_entity = entity_registry.async_get(hass).async_get(entry.data['climate_entity'])
+
     hass.data.setdefault(DOMAIN, {})
     hass.data[DOMAIN][entry.entry_id] = coordinator = OptisparkDataUpdateCoordinator(
         hass=hass,
         client=OptisparkApiClient(
             postcode=entry.data['postcode'],
-            session=async_get_clientsession(hass),
-        ),
+            session=async_get_clientsession(hass)),
+        climate_entity=climate_entity
     )
     # https://developers.home-assistant.io/docs/integration_fetching_data#coordinated-single-api-poll-for-data-for-all-entities
     await coordinator.async_config_entry_first_refresh()
